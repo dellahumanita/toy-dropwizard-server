fce3840379edbcbcca49365c999cd9f15a8feacd
Della <humanita.1999@gmail.com>
2022-03-02 18:24:22 -0700

    trying again

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
 
     @Override
     public String getName() {
-        return "hellooooooo-world";
+        return "bibbidy-bobbidy";
     }
 
     @Override
