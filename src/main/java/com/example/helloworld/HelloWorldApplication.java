7aaf66c347af5351182565f7533354449af732a7
Della <humanita.1999@gmail.com>
2022-03-02 18:17:31 -0700

    minor change to test rollback

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
 
     @Override
     public String getName() {
-        return "hello-world";
+        return "hellooooooo-world";
     }
 
     @Override
